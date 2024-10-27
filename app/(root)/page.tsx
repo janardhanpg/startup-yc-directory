e8f3523a5dfe6b192f3822204b937e8d48950468
Janardhan <janardhanpg12@gmail.com>
2024-10-28 01:01:43 +0530

    Add Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <>HOME</>;
+  return (
+    <>
+      <section className="pink_container " style={{background:'#EE2B69'}}>
+      <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
+    </>
+  );
 }
+
+
+
