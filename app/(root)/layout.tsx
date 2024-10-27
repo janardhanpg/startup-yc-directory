ef122ff1ad66e66e49d0953ccad82acc22620717
Janardhan <janardhanpg12@gmail.com>
2024-10-28 00:12:07 +0530

    Set up authentication

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import Navbar from "../components/Navbar";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <main className="font-work-sans">
+    
+    <Navbar />{children}</main>;
+}
