a8a9227d97c6cf87b57535dc25f01f7f21358c44
Janardhan <janardhanpg12@gmail.com>
2024-10-28 12:06:06 +0530

    setup startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartUpCard from "@/components/StartUpCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1,name: "Alex" },
+      _id: 1,
+      descripion: "This is a sample run",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container " style={{ background: "#EE2B69" }}>
@@ -18,6 +32,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartUpCardType, index: number) => (
+              <StartUpCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-resilts">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatDate(date: string){
+  return new Date(date).toLocaleDateString( 'en-UK' , {
+    day:'numeric',month:'long',  year: 'numeric'
+  })
+}
